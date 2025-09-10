4dab57ef392a3e9ba71657e2fb6c1b6a91782d45
fun2tube <pankajyadi@gmail.com>
2025-09-10 12:39:10 +0530

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to TechYaadi</h1>
+      <p>Your ultimate travel and tour management platform.</p>
+    </div>
+  );
+}
